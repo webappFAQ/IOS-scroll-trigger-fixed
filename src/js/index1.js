5b8c73d46f4f4513f092347b5fd82bf79866532a
rahul <ro87630872@126.com>
2016-11-09 10:17:27 +0800

    [UPD] 更新例子1

---
+++ src/js/index1.js
@@ -0,0 +1,14 @@
+
+(function(){
+    var currentTop = $('.title').offset().top;
+    function onScroll(){
+        console.log(12345511);
+        var top = document.body.scrollTop || document.documentElement.scrollTop;
+        if(top >= (currentTop)) {
+            $('.title').attr('style','position: fixed;top:0');
+        } else {
+            $('.title').attr('style','position: relative;top:auto');
+        }
+    }
+    window.addEventListener('scroll', onScroll);
+})()
